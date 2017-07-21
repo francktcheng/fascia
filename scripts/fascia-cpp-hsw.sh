56143271a252038eb4f0ba502d9d3ef0b256f6c9
lc37 <lc37@indiana.edu>
2017-07-20 22:09:13 -0400

    add affinity setup

--- scripts/fascia-cpp-hsw.sh
+++ scripts/fascia-cpp-hsw.sh
@@ -8,12 +8,25 @@ data_name=nyc.graph
 # template names
 # template=u3-1.fascia 
 template=u5-1.fascia 
-
 #u3-1.t u5-1.t u7-1.t # u5-2.t u5-3.t u7-1.t #u10-1.t
-thd=16
-itr=1
 
-log_loc=/N/u/lc37/Project/Harp-Graph-Counting/Results/Fascia-Native
+# thd=12
+# thd=24
+thd=48
+
+itr=10
+# itr=1000
+# itr=10000
+
+core_num=24 
+
+# affinity_typ=compact
+affinity_typ=scatter
+
+
+# log_loc=/N/u/lc37/Project/Harp-Graph-Counting/Results/Fascia-Native
+# log_loc=/N/u/lc37/Project/Harp-Graph-Counting/Results/LRT-Test
+log_loc=/N/u/lc37/Project/Harp-Graph-Counting/Results/Thd-Scale
 
 # if [ -f $out_log ]; then
 #     echo "remove old logs"
@@ -34,13 +47,17 @@ log_loc=/N/u/lc37/Project/Harp-Graph-Counting/Results/Fascia-Native
 # -h  Print this
 
 export OMP_NUM_THREADS=$thd
+export KMP_AFFINITY=granularity=fine,$affinity_typ 
 
 ## experiments on innerloop parallel
 for exec in fascia fascia-O3 
 do
-    echo "Start running $exec innerloopParal"
-../fascia-1.0/$exec -g $data_loc/graphs/$data_name -t $data_loc/templates/$template -i $itr -r -v >$log_loc/Res-$exec-graph_$data_name-template_$template-thd$thd-itr$itr.log 
-    echo "Finish running $exec innerloopParal"
+    for round in 1
+    do
+    echo "Start running $exec on $data_name with template $template thd_num: $thd core_num: $core_num Itr: $itr Affinity: $affinity_typ round: $round"
+../fascia-1.0/$exec -g $data_loc/graphs/$data_name -t $data_loc/templates/$template -i $itr -r -v >$log_loc/Res-$exec-graph_$data_name-template_$template-thd$thd-core$core_num-itr$itr-$affinity_typ-round$round.log 
+    echo "End running $exec on $data_name with template $template thd_num: $thd core_num: $core_num Itr: $itr Affinity: $affinity_typ round: $round"
+    done
 done
 
 # ## experiments on outerloop parallel
